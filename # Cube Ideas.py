795c93da1d23260cfff95fe0ab9416d1a6cd0b0d
minecraftpch <156861718+minecraftpch@users.noreply.github.com>
2024-01-17 11:02:48 -0600

    Add files via upload

---
+++ # Cube Ideas.py
@@ -0,0 +1,22 @@
+# Cube Ideas
+def flaglast3picks(card):
+    #Possible function to flag a card if it continues to go in the last 3 picks of a pack 
+    #if each pack for each player is a seperate list(?) of 15 cards on pick 1, 14 on pick 2, etc. Record each element in the lists that has length 3 or sum like that
+    #Disregard 2 champion added cards
+def nevermainboard(card):
+    #possible function to flag a card if it continues to not make mainboards (When its color is shared with some percentage of other cards in the pool)
+    #Disregard 2 champion added cards
+
+def winrate(decks): 
+    #possible function to check winrates of each 2c pair and an "other" cateogry 
+
+def lands(lands): 
+    #possible function to determine when lands are being picked on avg 
+
+def pickavg(card): 
+    #Possible function to find when a card is picked on average 
+    #Disregard 2 champion added cards
+
+
+
+
